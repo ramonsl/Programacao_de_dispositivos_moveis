fa23f97756ca1b84a41d92747ee28412036758b8
ramonsl <ramonsl@gmail.com>
2020-05-26 20:39:17 -0300

    Covid
    Exemplo Aplicação Covid.

---
+++ Celula_03/Bloco_03/CovidWorld/app/src/main/java/com/lopes/covidworld/activitys/all_states.kt
@@ -0,0 +1,110 @@
+package com.lopes.covidworld
+
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.AsyncTask
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lopes.covidworld.activitys.HomeActivity
+import kotlinx.android.synthetic.main.activity_all_states.*
+
+class all_states : AppCompatActivity() {
+
+    private var listStates = arrayListOf<States>()
+    private var adapter = StatesAdapter(listStates)
+    private var asyncTask : StatesTask? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_states)
+        setSupportActionBar(findViewById(R.id.appBar))
+        CarregaDados()
+
+        rvDados.layoutManager = GridLayoutManager(this,2)
+        rvDados.itemAnimator = DefaultItemAnimator()
+        rvDados.adapter = adapter
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Clique sobre algum estado para mais informações!")
+        builder.setPositiveButton("OK") { _: DialogInterface, i: Int -> }
+        builder.show()
+
+
+        appBar.setNavigationOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@all_states, HomeActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+    }
+
+    fun showProgress(show: Boolean){
+        if(show){
+            txtMsg.text = "Carregando..."
+        }else{
+            txtMsg.visibility = if(show) View.VISIBLE else View.GONE
+            progressBar.visibility = if(show) View.VISIBLE else View.GONE
+        }
+    }
+
+    fun CarregaDados(){
+        listStates.clear()
+        if(listStates.isNotEmpty()){
+            showProgress(false)
+        }else{
+            if(asyncTask==null){
+                if(HttpAllStates.hasConnection(this)){
+                    if(asyncTask?.status != AsyncTask.Status.RUNNING){
+                        asyncTask = StatesTask()
+                        asyncTask?.execute()
+                    }
+                }else{
+                    progressBar.visibility = View.GONE
+                }
+            }else if(asyncTask?.status==AsyncTask.Status.RUNNING){
+                showProgress(true)
+            }
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class StatesTask: AsyncTask<Void, Void, List<States?>>(){
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgress(true)
+        }
+
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun doInBackground(vararg params: Void?): List<States>? {
+            return HttpAllStates.loadStates()
+        }
+
+        private fun update(result: List<States>?){
+            if(result != null){
+                listStates.clear()
+                listStates.addAll(result.reversed())
+            }else{
+                txtMsg.text = "Erro ao Carregar"
+            }
+            adapter.notifyDataSetChanged()
+            asyncTask = null
+        }
+
+        override fun onPostExecute(result: List<States?>?) {
+            super.onPostExecute(result)
+            showProgress(false)
+            update(result as List<States>?)
+        }
+    }
+
+}
---
+++ Celula_03/Bloco_03/CovidWorld/app/src/main/java/com/lopes/covidworld/ws/HttpAllStates.kt
@@ -0,0 +1,89 @@
+package com.lopes.covidworld
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
+
+object HttpAllStates {
+
+    val urlStates ="https://covid19-brazil-api.now.sh/api/report/v1"
+
+
+    fun hasConnection(ctx: Context): Boolean{
+        val cm = ctx.getSystemService(Context.CONNECTIVITY_SERVICE)
+                as ConnectivityManager
+        val info =  cm.activeNetworkInfo
+        return info != null && info.isConnected
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun loadStates(): List<States>?{
+
+        val state = OkHttpClient.Builder()
+            .readTimeout(5, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .build()
+
+        val request = Request.Builder()
+            .url(urlStates)
+            .build()
+
+        val response = state.newCall(request).execute()
+        val jsonString = response.body?.string()
+
+        val jsonObject = JSONObject(jsonString)
+        val jsonArray = jsonObject.getJSONArray("data")
+
+        return readState(jsonArray)
+
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun readState(json: JSONArray) : List<States>?{
+        val estados = arrayListOf<States>()
+        try {
+            for (i in 0 .. json.length()-1) {
+                var js = json.getJSONObject(i)
+
+                val dia = formatarData(js.getString("datetime").substring(0,10))
+                val hora = js.getString("datetime").substring(11,16)
+
+                var states = States(
+                    js.getString("state"),
+                    js.getInt("cases"),
+                    js.getInt("deaths"),
+                    js.getInt("suspects"),
+                    js.getInt("refuses"),
+                    dia,
+                    hora
+                )
+                estados.add(states)
+            }
+        }catch (e: IOException) {
+            Log.e("Erro", "Impossivel ler JSON")
+        }
+
+        return estados
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun formatarData(data: String): String {
+        val diaString = data
+        var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        var date = LocalDate.parse(diaString)
+        var formattedDate = date.format(formatter)
+        return formattedDate
+    }
+
+}
